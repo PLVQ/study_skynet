57f4d6ff15a5ef35d90503629bd92ba627d51e40
pengjiang <1114307274@qq.com>
2021-03-04 15:58:52 +0800

    文件名修改

--- service/test/test.lua
+++ service/test/test.lua
@@ -2,5 +2,10 @@ local skynet = require "skynet"
 
 skynet.start(function()
     skynet.error("new server!")
-    skynet.error("test server")
+    skynet.error("test1 server")
+    skynet.newservice("test")
+    if not skynet.getenv "daemon" then
+		local console = skynet.newservice("console")
+	end
+    skynet.exit()
 end)
